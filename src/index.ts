19c5466a71224317c0454607c89f3bc18dfe7a06
den59k <den59k@gmail.com>
2024-04-13 18:02:49 +0500

    fix: fix types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { sc } from './fastify'
 
 export { unfoldSchema, schema } from './main'
 export { combineSchema, sc } from './fastify'
-
+export type { SchemaType, SchemaItem } from './types'
