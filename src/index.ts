bfa3a0a732cf66926aa6d86b39316044496f9b67
den59k <den59k@gmail.com>
2025-01-16 10:27:07 +0500

    fix: fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { unfoldSchema, schema } from './main'
+export { unfoldSchema, schema, registerAlias } from './main'
 export { combineSchema, sc } from './fastify'
 export type { CompactJsonSchemaProvider }from './fastify'
 export type { SchemaType, SchemaItem, SchemaAliases } from './types'
